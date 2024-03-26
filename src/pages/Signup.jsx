9a6d7cfbf259fe091fcdd575f892810f47a22420
Infant Franklin <infsntfrsnklin1@gmail.com>
2024-03-26 23:15:51 +0530

    UI changes on hompage andd signup pag

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="///"
+        />
+        <div className="bg-black/70 fixed h-screen w-full top-0 left-0" />
+
+        <div className="fixed w-full px-4 py-24 z-20">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/80 rounded-lg">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className=" text-3xl font-nsans-bold">Sign Up</h1>
+
+              <form action="" className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="email"
+                  autoComplete="email"
+                />
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="password"
+                  autoComplete="current-password"
+                />
+                <button className=" bg-red-600 py-3 my-6 rounded font-nsans-bold">
+                  Sign Up
+                </button>
+
+                <div className="flex items-center justify-between text-gray-600">
+                  <p>
+                    <input type="checkbox" className="mr-2" />
+                    Remember Me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+
+                <p className="my-4">
+                  <span className="text-gray-600 mr-2">
+                    Already Subscribed to Netflix?
+                  </span>
+                  <Link to="/login">Sign In</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
--- src/services/movieServices.js
+++ src/services/movieServices.js
@@ -2,11 +2,15 @@ const key = import.meta.env.VITE_TMDB_KEY;
 const baseUrl = "https://api.themoviedb.org/3";
 
 const endpoints = {
-    popular: `${baseUrl}/movie/popular?api_key=${key}`,
-    topRated: `${baseUrl}/movie/top_rated?api_key=${key}`,
-    trending: `${baseUrl}/movie/popular?api_key=${key}&language=en-US&page=2`,
-    comedy: `${baseUrl}/movie/popular?api_key=${key}&language=en-US&query=comedy&page=1&include_adult=false`,
-    upcoming: `${baseUrl}/movie/upcoming?api_key=${key}`,
+  popular: `${baseUrl}/movie/popular?api_key=${key}`,
+  topRated: `${baseUrl}/movie/top_rated?api_key=${key}`,
+  trending: `${baseUrl}/movie/popular?api_key=${key}&language=en-US&page=2`,
+  comedy: `${baseUrl}/movie/popular?api_key=${key}&language=en-US&query=comedy&page=3&include_adult=false`,
+  upcoming: `${baseUrl}/movie/upcoming?api_key=${key}&page=2`,
 };
 
-export default endpoints
+export function createImageUrl(filename, size) {
+  return `https://image.tmdb.org/t/p/${size}/${filename}`;
+}
+
+export default endpoints;
